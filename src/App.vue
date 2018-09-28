084bce3672422b0c62aabc17c5354b4a25521e42
Janos Perge <janos.perge@innonic.com>
2018-09-28 22:07:48 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+<style lang="scss">
+  @import './scss/variables.scss';
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
+<style>
+#app {
+  font-family: 'Raleway', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <h1>Mini-webshop with Vue.js</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">Bal
+          <div class="hoki">
+            <div class="hoki-title">asdasd</div>
+          </div>
+        </div>
+        <div class="col-md-6">Jobb</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  .hoki {
+    &-title {
+      color: red;
+    }
+  }
+</style>
